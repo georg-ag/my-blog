282dbc67701f059f3a7e64f85feb38d0f58b886c
Georg Adler-Gunzelmann <georg.adler.g@gmail.com>
2021-08-06 16:56:02 +0200

    feat(navigation): add nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './NavBar';
 import AboutPage from './pages/AboutPage';
 import ArticlePage from './pages/ArticlePage';
 import ArticlesList from './pages/ArticlesList';
@@ -10,6 +11,7 @@ function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar />
         <div id="page-body">
           <Route path="/" component={HomePage} exact />
           <Route path="/about" component={AboutPage} />
